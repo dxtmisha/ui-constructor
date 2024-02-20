15394c93f300282ebff059c7f363e6b5d13ca74d
dxtmisha <9943154+dxtmisha@users.noreply.github.com>
2024-02-21 01:09:27 +0700

    Добавлены базовые composables

---
+++ composables/ref/useCookieRef.ts
@@ -0,0 +1,40 @@
+import {
+  type Ref,
+  ref,
+  type UnwrapRef,
+  watch
+} from 'vue'
+
+import {
+  Cookie,
+  type CookieOptions
+} from '../../classes/Cookie'
+
+/**
+ * Creates a reactive variable to manage cookies.<br>
+ * Создает реактивную переменную для управления cookie.
+ * @param name cookie name /<br>название cookie
+ * @param defaultValue value or function to change data /<br>значение или функция для изменения данных
+ * @param options additional parameters /<br>дополнительные параметры
+ */
+export function useCookieRef<T> (
+  name: string,
+  defaultValue?: T | string | (() => (T | string)),
+  options?: CookieOptions
+): Ref<UnwrapRef<T> | string | undefined> {
+  if (name in items) {
+    return items[name]
+  }
+
+  const cookie = new Cookie<T>(name)
+  const item = ref(cookie.get(defaultValue, options))
+
+  watch(item, value => {
+    cookie.set(value as T, options)
+  })
+
+  items[name] = item
+  return item
+}
+
+const items: Record<string, Ref<any>> = {}
---
+++ composables/ref/useTranslateRef.ts
@@ -0,0 +1,39 @@
+import {
+  type ShallowRef,
+  shallowRef,
+  watchEffect
+} from 'vue'
+
+import { GeoRef } from '../../classes/ref/GeoRef'
+import {
+  Translate,
+  type TranslateList
+} from '../../classes/Translate'
+
+/**
+ * Getting the translated text by an array of keys or a string with a key.<br>
+ * Получение переведенного текста по массиву ключей или строке с ключом.
+ * @param names a string or an array with keys /<br>строка или массив с ключами
+ */
+export function useTranslateRef<
+  T extends string[]
+> (
+  names: T
+): ShallowRef<TranslateList<T>> {
+  const translate = shallowRef<TranslateList<T>>({} as TranslateList<T>)
+
+  watchEffect(async () => {
+    if (GeoRef.getLanguage()) {
+      translate.value = await Translate.getList(names)
+    }
+  })
+
+  return translate
+}
+
+/**
+ * Getting the translated text by an array of keys or a string with a key.<br>
+ * Получение переведенного текста по массиву ключей или строке с ключом.
+ * @param names a string or an array with keys /<br>строка или массив с ключами
+ */
+export const t = <T extends string[]> (names: T) => useTranslateRef(names)
