46d1f45df93cb8cbb1149983c921570487433011
dxtmisha <9943154+dxtmisha@users.noreply.github.com>
2024-03-10 18:01:39 +0700

    Удаление лишнего

--- packages/mutation/src/main.ts
+++ packages/mutation/src/main.ts
@@ -5,7 +5,4 @@ import App from './App.vue'
 
 const app = createApp(App)
 
-registrationUiMain(app).then(() => {
-  console.log('app-mutation')
-  app.mount('#ui-app-mutation')
-})
+registrationUiMain(app).then(() => app.mount('#ui-app-mutation'))
